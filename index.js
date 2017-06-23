2c78a864759713a3b00086814d87d725082e3c99
Jam Frigillana <jam@lbcx.ph-at-795040732541>
2017-06-23 10:41:19 +0800

    Registration page

--- index.js
+++ index.js
@@ -1,21 +1,41 @@
 var express = require('express');
 var app = express();
-var handlebars = require('handlebars');
+var expressSession = require('express-session');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 var hbs = require('hbs');
-
-
+var mongoose = require('mongoose');
+var hbsutils = require('hbs-utils')(hbs);
+var routes = require('./lib/routes');
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost/lmb');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('Connected to MongoDB.');
+});
+// Set port to 5000
 app.set('port', (process.env.PORT || 5000));
-
 app.use(express.static(__dirname + '/public'));
-
+app.use(expressSession({
+    secret: 'lmb2017',
+    saveUninitialized: true,
+    resave: true,
+}));
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({
+        extended: true
+    }))
+    .use(bodyParser.json());
+app.use('/node_modules', express.static(__dirname + '/node_modules'));
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'hbs');
+hbsutils.registerPartials(__dirname + '/views/partials');
+hbsutils.registerWatchedPartials(__dirname + '/views/partials');
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
-});
+routes(app);
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+  console.log('LMB is running on port', app.get('port'));
 });
