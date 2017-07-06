6899d0756bbe376820b402ce75c7be85e4648e6f
Jasmin Frigillana <jasyfrig@gmail.com>
2017-07-07 07:08:24 +0800

    saving to database

--- public/js/register.js
+++ public/js/register.js
@@ -1,17 +1,35 @@
-var database = firebase.database();
-console.log(database);
-
 $(document).ready(function() {
+    var database = firebase.database();
+
+    function writeUserData(details) {
+        	database.ref('users/' + details.nickname).set(details)
+            .then(function() {
+                alert('Registered!');
+            })
+            .catch(function() {
+                alert('Failed to register!');
+            });
+    }
+
     $('#register').submit(function(event) {
         var details = [];
 
         $('#register input, #register select').each(
             function(index) {
                 var input = $(this);
-                details[input.attr('name')] = input.val();
+                if (input.attr('name') === 'areaCode') {
+                    details['telephoneNumber'] = '(' + input.val() + ')';
+                } else if (input.attr('name') === 'telephone') {
+                    details['telephoneNumber'] += ' ' + input.val();
+                } else {
+                    details[input.attr('name')] = input.val();
+                }
+
                 event.preventDefault();
             }
         );
+
+        writeUserData(details);
 
         console.log(details);
     });
