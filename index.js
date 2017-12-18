b07325e60ac4a5725ef88b127d9fff2ceef981c6
Ma. Jamaica Frigillana <jamfrigillana@maica.lan>
2017-12-18 14:18:02 +0800

    send email

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ var hbs = require('hbs');
 var hbsutils = require('hbs-utils')(hbs);
 var routes = require('./lib/routes');
 var mysql = require('mysql');
+var nodemailer = require('nodemailer');
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
@@ -37,6 +38,44 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'hbs');
 hbsutils.registerPartials(__dirname + '/views/partials');
 hbsutils.registerWatchedPartials(__dirname + '/views/partials');
+
+// Generate test SMTP service account from ethereal.email
+// Only needed if you don't have a real mail account for testing
+nodemailer.createTestAccount((err, account) => {
+
+    // create reusable transporter object using the default SMTP transport
+    var transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+            user: 'jasyfrigi@gmail.com',
+            pass: 'Vansoffthewall'
+        }
+    });
+
+    // setup email data with unicode symbols
+    var mailOptions = {
+        from: '"Fred Foo 👻" <jamfrig@gmail.com>', // sender address
+        to: 'jamfrig@gmail.com', // list of receivers
+        subject: 'Hello ✔', // Subject line
+        text: 'Hello world?', // plain text body
+        html: '<b>Hello world?</b>' // html body
+    };
+
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message sent: %s', info.messageId);
+        // Preview only available when sending through an Ethereal account
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+        // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@blurdybloop.com>
+        // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+    });
+});
 
 routes(app, connection);
 
